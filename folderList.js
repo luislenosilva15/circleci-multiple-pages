c06612d5e2fa94e0b95aa65dc5d48e223a3037b5
Luis Leno <luizleno15@gmail.com>
2023-07-29 19:56:08 -0300

    adjust

---
+++ folderList.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+const directoryPath = 'pages';
+
+fs.readdir(directoryPath, { withFileTypes: true }, (err, files) => {
+  if (err) {
+    console.error('Erro ao ler o diretório:', err);
+    return;
+  }
+
+  const folders = files
+    .filter(file => file.isDirectory())
+    .map(file => file.name);
+
+  const json = JSON.stringify({ folders });
+
+  fs.writeFile('folders.json', json, 'utf8', err => {
+    if (err) {
+      console.error('Erro ao gravar o arquivo:', err);
+      return;
+    }
+
+    console.log('Arquivo JSON gerado com sucesso!');
+  });
+});
