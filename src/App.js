7341cad3a3b5a5c14488be7e6e3d99f5df3a5e5d
Luis Leno <luizleno15@gmail.com>
2023-08-01 10:15:53 -0300

    feat: adjust

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src22dsds2/Awpp.js</code> and save to reload.
+          Edit <code>src22dddsds2/Awpp.js</code> and save to reload.
         </p>
         <a
           className="App-link"
